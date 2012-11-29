670c7f59636dfa16e3aa4b53a0b29c1bc4b58ca4
Julien Bourdeau <julien@sigerr.org>
2012-11-29 23:23:24 +0100

    added new icons and rewrite everything in a function

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 /****************************************************************************
  *																			*
  *	 This file is suppose to replace the apache default index.php.			*
- * 	It displays folders and file in a folder and link to them, it is a 		*
- * 	simple standalone file, move it into your folders.						*
+ * 	It displays folders and files in the current folder and link to them.   *
+ * 	It is a simple standalone file, move it into your folders.				*
  *																			*
  * 	@Author: Julien Bourdeau <julien@sigerr.org>							*	
  * 	@URI: http://sigerr.org/												*			
  * 	@Since: August 2011														*
- * 	@Version: 1.0															*
+ * 	@Version: 2.0															*
  *																			*
  ****************************************************************************/
 
@@ -16,22 +16,53 @@
 /*
  *   Set useful variable
  **************************************************/
-	define('URL_IMG', 'http://sigerr.org/img/');
+	define('CDN_URL', 'http://cdn.sigerr.org/');
 	$cur_dir = urldecode(basename($_SERVER['REQUEST_URI']));
-	//print_r($_SERVER);
-	$url_folder_img = URL_IMG."folder.png";
-	$url_file_img = URL_IMG."file.png";
+	$url_folder_img = CDN_URL."folder.png";
+	//$url_file_img = CDN_URL."file.png";
+	$file_icon_list = array('aac', 'ai', 'aiff', 'avi', 'css', 'doc', 'docx', 'gif', 'gzip', 'html', 'jpeg', 'jpg', 'js', 'ma', 'mov', 'mp', 'mpeg', 'mpg', 'mv', 'pdf', 'php', 'png', 'psd', 'raw', 'rtf', 'tar', 'tiff', 'txt', 'wav', 'wmv', 'zip');
 	
 /*
  *   Define Functions
  **************************************************/
 
-function name($name) {
-	if(strlen($name) > 42) {
-		$name = substr($name, 0, 42);
+function get_file_info($file) {
+	$data = array();
+	global $file_icon_list;
+
+	//Set Name
+	$filename = pathinfo($file);
+	if(strlen($filename['filename']) > 42) {
+		$name = substr($name, 0, 36);
 		$name .= "...";
+		$name .= substr($name, -1, 3);
+		$name .= ".".$filename['extension'];
+	} else {
+		$name = $file;
 	}
-	return $name;
+	$data['name'] = $name;
+
+	//Set Icon
+	if ( in_array($filename['extension'], $file_icon_list) ) {
+		$data['icon'] = CDN_URL . $filename['extension'] . ".png";
+	} else {
+		$data['icon'] = CDN_URL . "generic.png";
+	}
+
+	
+	//Set Size
+	$size = filesize($file);
+	if ($size >= 1073741824) { // 1024x1024x1024
+		$size_str = round($size/1073741824, 1) . ' Go';
+	} elseif ($size >= 1048576) { // 1024x1024
+		$size_str = round($size/1048576, 1) . ' Mo';
+	} else {
+		$size_str = round($size/1024, 1) . ' Ko';
+	}
+	//$data['size'] = $size;
+	$data['size_str'] = $size_str;
+
+	return $data;
 }
  
  
@@ -116,12 +147,12 @@ function name($name) {
 
 	  <div id="container">
         <div id="header">
-    		<a href="http://sigerr.org/" class="left"><img border='0' src="<?php echo URL_IMG; ?>sigerr.org.png" /></a>
+    		<a href="http://sigerr.org/" class="left"><img border='0' src="<?php echo CDN_URL; ?>sigerr.org.png" /></a>
             
             <div id="email" class="right">
             	<table>
                 	<tr><td>
-            		<img src="<?php echo URL_IMG; ?>email.png" />
+            		<img src="<?php echo CDN_URL; ?>email.png" />
                     </td><td>  
                     <a href="mailto:julien@sigerr.org"><span class="htext">julien@sigerr.org</span></a>
                     </td></tr>
@@ -174,20 +205,18 @@ function name($name) {
 						$count = 1;
 						foreach(glob('*.*') as $file){
 							if($file == "index.php") continue; //dont display the index.php
+
+							$data = get_file_info($file);
+
 							echo "<tr class='";
 									echo ($count%2 == 0)?"even":"odd";
 									echo "'> \r\n";
-								echo "\t <td> \r\n \t\t <img src='$url_file_img' />\r\n \t </td> \r\n";
+								echo "\t <td> \r\n \t\t <img src='". $data['icon'] ."' />\r\n \t </td> \r\n";
 								echo "\t <td> \r\n \t\t <a href='$file' title='$file'> ";
-									echo name($file);
+									echo $data['name'];
 									echo "</a>\r\n \t </td> \r\n";
 								echo "\t <td>";
-									$size = filesize($file);
-									if($size >= 1048576) { // 1024x1024
-										echo round($size/1048576, 1); echo ' Mo';
-									}else{
-										echo round($size/1024, 1); echo ' Ko';
-									}
+									echo $data['size_str'];
 									echo "\r\n \t </td> \r\n";
 							echo "</tr> \r\n";
 							$count++;
